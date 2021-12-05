a741dbe9becfef10e427a2111f781addc2cec605
KirMaid <ialavash228@gmail.com>
2021-12-06 00:52:45 +0300

    feat: add page in my project block, fix burger.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 setTimeout(function() {
     document.body.classList.add('body_visible');
 }, 200);
+
+function hide() {
+    document.body.querySelector('.menu__box').style.left = "-100%"; // = "-100%";
+    document.body.querySelector('.menu__box').style.visibility = "hidden";
+    document.body.querySelector('#menu__toggle').checked = false;
+    //alert(document.body.querySelector('#menu__toggle').checked);
+};
+
+function show() {
+    //alert(document.body.querySelector('#menu__toggle').checked);
+    if (document.body.querySelector('#menu__toggle').checked == false) {
+        document.body.querySelector('.menu__box').style.left = "-100%"; // = "-100%";
+        document.body.querySelector('.menu__box').style.visibility = "hidden";
+    } else {
+        document.body.querySelector('.menu__box').style.left = "0"; // = "-100%";
+        document.body.querySelector('.menu__box').style.visibility = "visible";
+    }
+
+};
